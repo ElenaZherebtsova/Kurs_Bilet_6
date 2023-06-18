98e013a790c9fe6374117e39fdf62ea95076d4e1
Elena <lenulka.90@mail.ru>
2023-06-18 10:58:12 +0300

    Возведение в квадрат.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-
-
-        System.out.println("Hello world!");
+        System.out.println("Квадрат числа:");
         int a1 = 11;
         int a2 = a1 * a1;
         System.out.println("a2 = " + a2);
